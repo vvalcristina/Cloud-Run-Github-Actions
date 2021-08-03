f876e0fbfb5221fee0ea384fda67f69c519ed8f3
Valeria Silva <silvvavaleria@gmail>
2021-08-03 09:15:01 -0300

    Update workflow

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return "Hello Valeria!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
