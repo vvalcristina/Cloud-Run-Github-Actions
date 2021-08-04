829acd09535f6d7d8c040244d9e7695918308c42
Valeria Silva <silvvavaleria@gmail>
2021-08-04 09:56:08 -0300

    Include scrip.sh

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
-# Use the official lightweight Python image.
 # https://hub.docker.com/_/python
 FROM python:3.9-slim
 
-# Allow statements and log messages to immediately appear in the Knative logs
+# Allow statements and log messages to immediately appear in the Cloud Run logs
 ENV PYTHONUNBUFFERED True
+
+# Install production dependencies.
+RUN pip install Flask==1.1.2 gunicorn==20.0.4 google-cloud-bigquery
 
 # Copy local code to the container image.
 ENV APP_HOME /app
 WORKDIR $APP_HOME
-COPY . ./
+COPY *.py ./
 
-# Install production dependencies.
-RUN pip install Flask gunicorn
-
-# Run the web service on container startup. Here we use the gunicorn
-# webserver, with one worker process and 8 threads.
+# Run the web service on container startup. 
+# Use gunicorn webserver with one worker process and 8 threads.
 # For environments with multiple CPU cores, increase the number of workers
 # to be equal to the cores available.
-# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
+# [END eventarc_gcs_dockerfile]
--- app.py
+++ app.py
@@ -1,12 +1,53 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+import json
+from google.cloud import bigquery
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello Valeria!"
 
+# [END eventarc_gcs_server]
+
+
+# [START eventarc_gcs_handler]
+@app.route('/', methods=['POST'])
+def index():
+    # Gets the Payload data from the Audit Log
+    content = request.json
+    try:
+        print(content)
+        ds = content['resource']['labels']['dataset_id']
+        proj = content['resource']['labels']['project_id']
+        tbl = content['protoPayload']['resourceName']
+        rows = int(content['protoPayload']['metadata']['tableDataChange']['insertedRowsCount'])
+        if ds == 'cloud_run_tmp' and tbl.endswith('tables/cloud_run_trigger') and rows > 0:
+            query = create_agg()
+            return "table created", 200
+    except:
+        # if these fields are not in the JSON, ignore
+        pass
+    return "ok", 200
+
+
+# [END eventarc_gcs_handler]
+
+def create_agg():
+    client = bigquery.Client()
+    query = """
+CREATE OR REPLACE TABLE cloud_run_tmp.created_by_trigger AS
+SELECT 
+  name, SUM(number) AS n
+FROM cloud_run_tmp.cloud_run_trigger
+GROUP BY name
+ORDER BY n desc
+LIMIT 10
+    """
+    client.query(query)
+    return query
+
+
+# [START eventarc_gcs_server]
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+# [END eventarc_gcs_server]
